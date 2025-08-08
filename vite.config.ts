124ded777b91d3705d90a6e689464f39b1bbbef8
JAOS-DEV <jaosullivan0@gmail.com>
2025-08-08 04:32:25 +0100

    Update Content Security Policy in index.html and Vite configuration to enhance security and support blob URLs for scripts and workers.

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig(({ mode }) => {
     server: {
       headers: {
         "Content-Security-Policy":
-          "script-src 'self' 'unsafe-inline' https://esm.sh https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; frame-src 'self' https://accounts.google.com https://www.gstatic.com https://*.firebaseapp.com https://*.web.app; connect-src 'self' https://apis.google.com https://accounts.google.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://www.googleapis.com https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://firestore.googleapis.com https://firebasestorage.googleapis.com https://region1.google-analytics.com https://*.google-analytics.com;",
+          "script-src 'self' 'unsafe-inline' blob: https://esm.sh https://apis.google.com https://accounts.google.com https://www.gstatic.com https://www.googleapis.com https://www.googletagmanager.com; worker-src 'self' blob:; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; frame-src 'self' https://accounts.google.com https://www.gstatic.com https://*.firebaseapp.com https://*.web.app; connect-src 'self' https://apis.google.com https://accounts.google.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://www.googleapis.com https://firebase.googleapis.com https://firebaseinstallations.googleapis.com https://firestore.googleapis.com https://firebasestorage.googleapis.com https://region1.google-analytics.com https://*.google-analytics.com;",
       },
     },
   };
